43880d8d61e63c8e74707397471c7c8034745029
hdminh247 <hdminh247@gmail.com>
2022-09-30 17:05:28 +0700

    Finish part 1

--- src/hooks/useCatImages.js
+++ src/hooks/useCatImages.js
@@ -1,14 +1,25 @@
-export function useCatImages(deps = []) {
-  let images = [];
+import { useState, useEffect} from "react";
 
-  try {
-    const images = fetch(
-      "https://api.thecatapi.com/v1/images/search?limit=5"
-    ).then((res) => res.json());
-  } catch (e) {
-    console.error("fetch failed!");
-    console.error(e);
+export function useCatImages(deps = []) {
+  const [images, setImages] = useState([]);
+
+  const getCatImages = () => {
+    try {
+      fetch(
+          "https://api.thecatapi.com/v1/images/search?limit=5"
+      ).then(async (res) => {
+        setImages(await res.json())
+      });
+    } catch (e) {
+      console.error("fetch failed!");
+      console.error(e);
+    }
   }
+
+  useEffect(()=>{
+    getCatImages();
+  },[])
+
 
   return images;
 }
